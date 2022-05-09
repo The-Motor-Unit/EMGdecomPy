da048629b4575da7ced121e9a2c2b3414b4fd2ed
Rowan Sivanandam <rowan.sivanandam@gmail.com>
2022-05-09 16:25:20 -0700

    test: adding test for extend_input_by_R git push

--- tests/test_emg-decompy.py
+++ tests/test_emg-decompy.py
@@ -1,1 +1,11 @@
 from emg-decompy import emg-decompy
+
+R = 5
+x = np.array([1,2,3,4,5,6,7])
+assert extend_input_by_R(x, R)[0][0] == x[0]
+assert extend_input_by_R(x, R)[0][-1] == 0
+assert len(extend_input_by_R(x, R)) == len(x)
+assert extend_input_by_R(x, R)[-1][0] == x[-1]
+assert extend_input_by_R(x, R)[0][0] == extend_input_by_R(x, R)[1][1]
+
+
