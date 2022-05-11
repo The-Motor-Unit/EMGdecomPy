7ead093560881e4405b4046fdb7c3efaf84c2fed
Rowan Sivanandam <rowan.sivanandam@gmail.com>
2022-05-11 12:19:38 -0700

    function: writing up code to subtract mean from extended matrix

--- src/emg-decompy/emg-decompy.py
+++ src/emg-decompy/emg-decompy.py
@@ -102,3 +102,79 @@ def extend_input_all_channels(x_mat, R):
         extended_x_mat[i] = extended_channel
         
     return extended_x_mat
+
+
+def subtract_channel_average(raw_flattened, extended_x_mat):
+    """
+    Takes a flattened matrix from flatten_signal function,
+    finds the mean of each channel, and subtracts it from 
+    each channel in the extended x matrix given by function
+    extend_all_input_channels.
+    
+    Parameters
+    ----------
+        raw_flattened: numpy.ndarray
+            2D array to find averages.
+        extended_x_mat: numpy.ndarray
+            array to subtract averages from.
+        
+    Returns
+    -------
+        numpy.ndarray
+            extended_x_mat with averages subtracted.
+        
+    Example:
+        >>> raw_flattened = np.array([[np.array([]),
+                                         np.array([[46.79361979, 13.22428385, -7.12076823, 26.44856771,
+                                                 50.86263021, 36.62109375]])                             ,
+                                         np.array([[76.29394531, 39.67285156, 29.50032552, 34.58658854,
+                                                 47.8108724 , 57.98339844]])                             ,
+                                         np.array([[74.2594401 ,  4.06901042, -5.08626302, 21.36230469,
+                                                 30.51757812, 36.62109375]])                             ,
+                                         np.array([[62.05240885, 28.48307292, 17.29329427,  3.05175781,
+                                                 22.37955729, -5.08626302]])                             ]],
+                                                 dtype=object)
+        >>> extended_x_mat = array([[[ 1.,  0.,  0.,  0.,  0.,  0.],
+                                    [ 2.,  1.,  0.,  0.,  0.,  0.],
+                                    [ 3.,  2.,  1.,  0.,  0.,  0.]],
+
+                                   [[ 4.,  0.,  0.,  0.,  0.,  0.],
+                                    [ 5.,  4.,  0.,  0.,  0.,  0.],
+                                    [ 6.,  5.,  4.,  0.,  0.,  0.]],
+
+                                   [[ 7.,  0.,  0.,  0.,  0.,  0.],
+                                    [ 8.,  7.,  0.,  0.,  0.,  0.],
+                                    [ 9.,  8.,  7.,  0.,  0.,  0.]],
+
+                                   [[10.,  0.,  0.,  0.,  0.,  0.],
+                                    [11., 10.,  0.,  0.,  0.,  0.],
+                                    [12., 11., 10.,  0.,  0.,  0.]]])
+
+    """
+    averages = []
+    for index, array in enumerate(raw_flattened):
+        averages.append(array.mean())
+    for channel, arr in enumerate(extended_x_mat):
+        for ext_index, ext_array in enumerate(extended_x_mat[channel]):
+            extended_x_mat[channel][ext_index] = extended_x_mat[channel][ext_index] - np.averages[channel]
+    return np.array(extended_x_mat)
+    
+
+
+SIG = np.array([[np.array([]),
+         np.array([[46.79361979, 13.22428385, -7.12076823, 26.44856771,
+                 50.86263021, 36.62109375]])                             ,
+         np.array([[76.29394531, 39.67285156, 29.50032552, 34.58658854,
+                 47.8108724 , 57.98339844]])                             ,
+         np.array([[74.2594401 ,  4.06901042, -5.08626302, 21.36230469,
+                 30.51757812, 36.62109375]])                             ,
+         np.array([[62.05240885, 28.48307292, 17.29329427,  3.05175781,
+                 22.37955729, -5.08626302]])                             ]], dtype=object)
+
+x = flatten_signal(SIG)
+
+extended = extend_input_all_channels(np.array([[1, 2, 3], [4, 5, 6],[7,8,9],[10,11,12]]), 5)
+
+subtract_channel_average(x, extended)
+
+
