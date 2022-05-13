98cfa16bc36207cd5d7f89aa4263d202ef750d61
Rada Rudyak <radawilinofsky@gmail.com>
2022-05-13 07:08:40 -0700

    fix: extend() function now outputs what we agreed upon

--- src/emg-decompy/emg-decompy.py
+++ src/emg-decompy/emg-decompy.py
@@ -3,15 +3,16 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
+
 def flatten_signal(raw):
     """
     Takes the raw EMG signal array, flattens it, and removes empty channels with no data.
-    
+
     Parameters
     ----------
     raw: numpy.ndarray
         Raw EMG signal array.
-        
+
     Returns
     -------
     numpy.ndarray
@@ -20,9 +21,12 @@ def flatten_signal(raw):
     # Flatten input array
     raw_flattened = raw.flatten()
     # Remove empty channels and then removes dimension of size 1
-    raw_flattened = np.array([channel for channel in raw_flattened if 0 not in channel.shape]).squeeze()
+    raw_flattened = np.array(
+        [channel for channel in raw_flattened if 0 not in channel.shape]
+    ).squeeze()
 
     return raw_flattened
+
 
 def extend_input_by_R(x, R):
     """
@@ -56,29 +60,30 @@ def extend_input_by_R(x, R):
     for i in range(R + 1):
         extended_x[i][i : i + len(x)] = x
 
-    # Transpose array and cut off extra R rows
-    extended_x = extended_x.T[0 : len(x)]
+    # Optional: Cut off extra R rows
+    extended_x = extended_x.T[0 : len(x)].T
 
     return extended_x
+
 
 def extend_input_all_channels(x_mat, R):
     """
     Takes an array with dimensions m by k,
     where m represents channels and k represents observations,
     and "extends" it to return an array of shape m by k by R+1.
-    
+
     Parameters
     ----------
         x_mat: numpy.ndarray
             2D array to be extended.
         R: int
             How far to extend x.
-        
+
     Returns
     -------
         numpy.ndarray
             m by k by R+1 extended array.
-        
+
     Example:
         >>> R = 5
         >>> x_mat = np.array([[1, 2, 3], [4, 5, 6]])
@@ -94,54 +99,55 @@ def extend_input_all_channels(x_mat, R):
     """
     extended_x_mat = np.zeros([x_mat.shape[0], x_mat.shape[1], R + 1])
     for i, channel in enumerate(x_mat):
-        
+
         # Extend channel
         extended_channel = extend_input_by_R(channel, R)
 
         # Add extended channel to the overall matrix of extended channels
         extended_x_mat[i] = extended_channel
-        
+
     return extended_x_mat
-  
+
+
 def whiten(x):
-    '''
+    """
     Whiten the input matrix. First, subtract the mean,
     then center the data, then perform zca whitening
-    
+
     Parameters
     ----------
         x: numpy.ndarray
             2D array to be whitened
-        
+
     Returns
     -------
         numpy.ndarray
             whitened array
-        
+
     Example
     --------
         >>> x = np.array([[5,6,7],  # Feature-1
               [23,29,31]]) # Feature-2
         >>> whiten(x)
-    
+
     Because this function takes such a long time and is the bottleneck of the process,
     it should be considered for optimization.
     One place to start: https://towardsdatascience.com/only-numpy-back-propagating-through-zca-whitening-in-numpy-manual-back-propagation-21eaa4a255fb
-    '''
-    
+    """
+
     # Subtract Average
     x_cent = x.T - np.mean(x.T, axis=0)
     x_cent = x_cent.T
-    
+
     # Calculate covariance matrix
-    cov_mat = np.cov(x_cent, rowvar=True, bias=True)    
-    
+    cov_mat = np.cov(x_cent, rowvar=True, bias=True)
+
     # Eigenvectors
     w, v = linalg.eig(cov_mat)  # values and vectors
-    
-    diagw = np.diag(1/(w**0.5)) # or np.diag(1/((w+.1e-5)**0.5))
+
+    diagw = np.diag(1 / (w ** 0.5))  # or np.diag(1/((w+.1e-5)**0.5))
     diagw = diagw.real.round(4)
 
     wzca = np.dot(np.dot(np.dot(v, diagw), v.T), x_cent)
-    
+
     return wzca
