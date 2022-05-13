62d839ad84c9ddd6d66d49ec141cf240dfbce419
Daniel King <dnlkng13@gmail.com>
2022-05-12 21:46:29 -0700

    fix: Import packages, reference package name in functions

--- tests/test_emgdecompy.py
+++ tests/test_emgdecompy.py
@@ -1,5 +1,6 @@
-from emgdecompy import emgdecompy
+from emgdecompy import emgdecompy as emg
 import random 
+import numpy as np
 
 def test_extend_input_by_R():
     """
@@ -9,33 +10,35 @@ def test_extend_input_by_R():
     R_two = 10
     x = np.array([1, 2, 3, 4, 5, 6, 7])
 
-    assert extend_input_by_R(x, R_one)[0][0] == x[0]
-    assert extend_input_by_R(x, R_one)[0][-1] == 0
-    assert extend_input_by_R(x, R_one).shape == (len(x), R_one + 1)
-    assert extend_input_by_R(x, R_one)[-1][0] == x[-1]
-    assert extend_input_by_R(x, R_one)[0][0] == extend_input_by_R(x, R_one)[1][1]
+    assert emg.extend_input_by_R(x, R_one)[0][0] == x[0]
+    assert emg.extend_input_by_R(x, R_one)[0][-1] == 0
+    assert emg.extend_input_by_R(x, R_one).shape == (len(x), R_one + 1)
+    assert emg.extend_input_by_R(x, R_one)[-1][0] == x[-1]
+    assert emg.extend_input_by_R(x, R_one)[0][0] == emg.extend_input_by_R(x, R_one)[1][1]
 
-    assert extend_input_by_R(x, R_two)[0][0] == x[0]
-    assert extend_input_by_R(x, R_two)[0][-1] == 0
-    assert extend_input_by_R(x, R_two).shape == (len(x), R_two + 1)
-    assert extend_input_by_R(x, R_two)[-1][0] == x[-1]
-    assert extend_input_by_R(x, R_two)[0][0] == extend_input_by_R(x, R_two)[1][1]
+    assert emg.extend_input_by_R(x, R_two)[0][0] == x[0]
+    assert emg.extend_input_by_R(x, R_two)[0][-1] == 0
+    assert emg.extend_input_by_R(x, R_two).shape == (len(x), R_two + 1)
+    assert emg.extend_input_by_R(x, R_two)[-1][0] == x[-1]
+    assert emg.extend_input_by_R(x, R_two)[0][0] == emg.extend_input_by_R(x, R_two)[1][1]
 
 def test_extend_input_all_channels():
     """
     Run unit tests on extend_input_all_channels function from emg-decomPy
     """
+    R_one = 5
+    R_two = 10
     x_mat = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-    assert extend_input_all_channels(x_mat, R_one).shape == (len(x_mat), len(x_mat[0]), R_one + 1)
-    assert extend_input_all_channels(x_mat, R_one)[0][0][0] == extend_input_all_channels(x_mat, R_one)[0][1][1]
-    assert extend_input_all_channels(x_mat, R_one)[0][0][-1] == 0
-    assert sum(extend_input_all_channels(x_mat, R_one)[-1][-1]) == sum(x_mat[-1])
+    assert emg.extend_input_all_channels(x_mat, R_one).shape == (len(x_mat), len(x_mat[0]), R_one + 1)
+    assert emg.extend_input_all_channels(x_mat, R_one)[0][0][0] == emg.extend_input_all_channels(x_mat, R_one)[0][1][1]
+    assert emg.extend_input_all_channels(x_mat, R_one)[0][0][-1] == 0
+    assert sum(emg.extend_input_all_channels(x_mat, R_one)[-1][-1]) == sum(x_mat[-1])
 
-    assert extend_input_all_channels(x_mat, R_two).shape == (len(x_mat), len(x_mat[0]), R_two + 1)
-    assert extend_input_all_channels(x_mat, R_two)[0][0][0] == extend_input_all_channels(x_mat, R_two)[0][1][1]
-    assert extend_input_all_channels(x_mat, R_two)[0][0][-1] == 0
-    assert sum(extend_input_all_channels(x_mat, R_two)[-1][-1]) == sum(x_mat[-1])
+    assert emg.extend_input_all_channels(x_mat, R_two).shape == (len(x_mat), len(x_mat[0]), R_two + 1)
+    assert emg.extend_input_all_channels(x_mat, R_two)[0][0][0] == emg.extend_input_all_channels(x_mat, R_two)[0][1][1]
+    assert emg.extend_input_all_channels(x_mat, R_two)[0][0][-1] == 0
+    assert sum(emg.extend_input_all_channels(x_mat, R_two)[-1][-1]) == sum(x_mat[-1])
 
 def create_emg_data(m=13, n=5, q=10):
     """
@@ -89,7 +92,7 @@ def test_flatten_signal():
         assert type(i) == np.ndarray, "Input is not type numpy.ndarray"
         assert i.shape != (1,1), "Input array is already one-dimensional."
 
-        flat = flatten_signal(i)
+        flat = emg.flatten_signal(i)
         
         # shape of fake data 
         m, n = i.shape
