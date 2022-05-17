79c08821d6206e747c92dcf93d20403f4e32bebf
Rada Rudyak <radawilinofsky@gmail.com>
2022-05-17 04:03:13 -0700

    feat: add orthogonalize, normalize, apply_contrast_fun_router, skew functions and skeleton of sepatation function

--- src/emgdecompy/emgdecompy.py
+++ src/emgdecompy/emgdecompy.py
@@ -190,3 +190,151 @@ def whiten(x):
     wzca = np.dot(np.dot(np.dot(v, diagw), v.T), x_cent)
 
     return wzca
+
+
+def orthogonalize(w, B):
+    '''
+    Step 2b from Negro et al: wi(n) = wi(n) - BB{t}*wi(n)
+    Note: this is not true orthogonalization, such as Gram–Schmidt process
+    This is dubbed in paper "source deflation procedure"
+    
+    Parameters
+    ----------
+        w: numpy.ndarray
+            vectors for which we seek orthogonal matrix
+        B: numpy.ndarray
+            matrix to 'deflate' w by
+        
+    Returns
+    -------
+        numpy.ndarray
+            'deflated' array
+        
+    Example
+    --------
+        >>> w = np.array([[5,6],  
+              [23,29]]) 
+        >>> B = np.array([[3,3],  
+              [3,3]]) 
+        >>> orthogonalize(w, B)
+    '''
+    w = w - np.dot(np.dot(B.T,w),B)
+    return w
+
+def normalize(w):
+    '''
+    Step 2c from Negro et al: wi(n) = wi(n)/||wi(n)||
+    
+    To normalize a matrix means to scale the values 
+    such that that the range of the row or column values is between 0 and 1
+    
+    Reference : https://www.delftstack.com/howto/numpy/python-numpy-normalize-matrix/
+    
+    Parameters
+    ----------
+        w: numpy.ndarray
+            vectors to normalize
+        
+    Returns
+    -------
+        numpy.ndarray
+            'normalized' array
+        
+    Example
+    --------
+        >>> w = np.array([[5,6],  
+              [23,29]])
+        >>> normalize(w)
+    '''
+    norms = np.linalg.norm(w)
+    w = w / norms
+    return w
+
+def apply_contrast_fun_router(w, fun = skew):
+    '''
+    Takes first derivitive and applies contrast function to w with map()
+    for Step 2a of fixed point algorithm
+    Options include functions mentioned in Negro et al
+    
+    Parameters
+    ----------
+        fun: str
+            name of contrast function to use
+        w: numpy.ndarray
+            matrix to apply contrast function to
+        
+    Returns
+    -------
+        numpy.ndarray
+            matrix with contrast function applied
+        
+    Example
+    --------
+        >>> w = np.array([1, 2, 3])
+        >>> fun = skew 
+        >>> apply_contrast_fun_router(w, fun)
+        >>> array([1, 4, 9])
+    '''
+    
+    #an_array = np.array([1, 2, 3])
+    
+    #def double(x):
+    #    return x * 2
+    
+    #mapped_array = double(an_array)
+    #print(mapped_array)
+    
+    rtn = fun(w)
+    return rtn
+
+def skew(x):
+    '''
+    Takes first derivitive and applies skew function to w
+    skew = x^3 / 3
+    
+    Parameters
+    ----------
+        x: numpy.ndarray
+            matrix to apply contrast function to
+        
+    Returns
+    -------
+        numpy.ndarray
+            matrix with contrast function applied
+        
+    Example
+    --------
+        >>> x = 4
+        >>> skew(x)
+        >>> 16
+    '''
+    
+    # first derivitive of x^3/3 = x^2
+    rtn = x**2
+    
+    return rtn
+
+def separation(z, Tolx):
+    '''
+    Parameters
+    ----------
+        z: numpy.ndarray
+            Product of whitened matrix W obtained in whiten() step and extended 
+        Tolx: numpy.ndarray
+            Tolx for element-wise comparison
+        
+    Returns
+    -------
+        numpy.ndarray
+            'deflated' array
+    '''
+    n = 0
+    while ()
+    
+    # calculate A
+    # A = average of 
+    
+    w = fixed_point_algo(w, z) #2a
+    w = orthogonalize(w, B) #2b
+    w = normalize(w) #2c
+    n = n+1 #2d
