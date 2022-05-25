0f0a0c18844b6ff137c48e51e6176e3a786f1c22
Daniel King <dnlkng13@gmail.com>
2022-05-25 10:34:16 -0700

    test: Add separation test

--- tests/test_decomposition.py
+++ tests/test_decomposition.py
@@ -1,5 +1,7 @@
-from emgdecompy import decomposition as emg
+import emgdecompy as emg
 import numpy as np
+from scipy.io import loadmat
+from scipy import linalg
 
 def test_normalize():
     """
@@ -26,6 +28,40 @@ def test_normalize():
         # normalize
         normalized = fake_data / frob_norm
 
-        fx = emg.normalize(fake_data)
+        fx = emg.decomposition.normalize(fake_data)
 
         assert np.allclose(normalized, fx), "Array normalized incorrectly."
+
+def test_separation():
+    """
+    Run unit test on separation function from EMGdecomPy.
+    """
+    Tolx = 10e-4
+    random_state = np.random.randint(100, 1000)
+    gl_10 = loadmat('data/raw/GL_10.mat')
+    signal = gl_10["SIG"]
+    signal = emg.preprocessing.flatten_signal(signal)
+    x = emg.preprocessing.center_matrix(signal)
+    x = emg.preprocessing.extend_all_channels(signal, 16)
+    z = emg.preprocessing.whiten(x)
+
+    n = 0
+    np.random.seed(random_state)
+    w_curr = np.random.rand(z.shape[0])
+    w_prev = np.random.rand(z.shape[0])
+    B = np.zeros((1088, 1))
+    max_iter = 10
+
+    while linalg.norm(np.dot(w_curr.T, w_prev) - 1) > Tolx:
+        b = np.dot(w_prev, z)
+        g_b = emg.contrast.apply_contrast(b)
+        A = (emg.contrast.apply_contrast(b, der=True)).mean()
+        w_curr = (z * g_b).mean(axis=1) - A * w_prev
+        w_curr = emg.decomposition.orthogonalize(w_curr, B)
+        w_curr = emg.decomposition.normalize(w_curr)
+        w_prev = w_curr
+        n += 1
+        if n > max_iter:
+            break
+                
+    assert (w_curr == emg.decomposition.separation(z, B, Tolx, emg.contrast.skew, max_iter, random_state)).all(), "Separation vector incorrectly calculated."
