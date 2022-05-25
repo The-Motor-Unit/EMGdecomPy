e6bd972981d994471e209b694d0ecc25b063e1a7
Daniel King <dnlkng13@gmail.com>
2022-05-25 16:03:41 -0700

    test: add initialize_w tests

--- tests/test_decomposition.py
+++ tests/test_decomposition.py
@@ -1,9 +1,18 @@
-from codecs import xmlcharrefreplace_errors
 import emgdecompy as emg
 import numpy as np
 from scipy.io import loadmat
 from scipy import linalg
 from test_preprocessing import create_emg_data
+
+def test_initialize_w():
+     """
+    Run unit tests on initialize_w function from EMGdecomPy.
+    """
+    x = np.array([[1, 2, 3, 4,], [5, 7, 9, 11], [12, 15, 18, 21]])
+    assert (initialize_w(x) == np.array([4, 11, 21])).all(), "Returned wrong column."
+
+    x = np.zeros((5, 5))
+    assert initialize(x).shape == np.zeros(5).shape, "Output contains wrong dimensions."
 
 def test_normalize():
     """
