3e5a0c3fb83c7ff54f690ea0dabe798340c67717
Jasmine Ortega <jorteg01@ubc.ca>
2022-06-09 10:42:29 -0700

    test: create ortho deflate test

--- tests/test_decomposition.py
+++ tests/test_decomposition.py
@@ -109,31 +109,24 @@ def test_separation():
     ).all(), "Separation vector incorrectly calculated."
 
 
-def test_orthogonalize():
+def test_deflate():
     """
-    Run unit tests on orthogonalize() function from EMGdecomPy.
+    Run unit tests on deflate function from EMGdecomPy.
     """
-    for i in range(0, 10):
-        x = np.random.randint(1, 100)
-        y = np.random.randint(1, 100)
-        z = np.random.randint(1, 100)
+    w_list = [np.array([1, 2, 3]), 
+              np.array([2, 4, 6])]
 
-        w = np.random.randn(x, y) * 1000
-        b = np.random.randn(x, z) * 1000
+    B_list = [np.array([[ 2,  4,  6],
+                       [10, 20, 30]]),
+             np.array([[ 1,  3,  4],
+                       [10, 20, 30]])]
 
-        assert (
-            b.T.shape[1] == w.shape[0]
-        ), "Dimensions of input arrays are not compatible."
-
-        # orthogonalize by hand
-        ortho = w - b @ (b.T @ w)
-
-        fx = emg.decomposition.orthogonalize(w, b)
-
-        assert np.array_equal(
-            ortho, fx
-        ), "Manually calculated array not equivalent to emg.orthogonalize()"
-        assert fx.shape == w.shape, "The output shape of w is incorrect."
+    answers = [np.array([-1455, -2910, -4365]),
+               np.array([-2836, -5710, -8546])]
+    
+    for i, answer in enumerate(answers):
+        fx_answer = emg.decomposition.deflate(w_list[i], B_list[i])
+        assert np.array_equal(fx_answer, answer), "Source deflated incorrectly."
 
 
 def test_refinement(random_seed=42):
