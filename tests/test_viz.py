fd20eef8056049bfbbf33d1c3b225f4104d697b1
Jasmine Ortega <jorteg01@ubc.ca>
2022-06-23 12:16:43 -0700

    test: add RMSE test

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -28,6 +28,23 @@ def fx_data():
     data = raw[1, 1:3]
     return data
 
+def test_RMSE():
+    """
+    Run unit test on RMSE function from EMGdecomPy.
+    """
+    actual = np.array([0, 10, 50, 75])
+    predicted = np.array([0.1, 10.1, 50.1, 75.1])
+    
+    # hand calculate MSE
+    mse = np.sum((actual - predicted) ** 2) / len(actual)
+    
+    rmse = np.sqrt(mse)
+    
+    rmse_fx = emgdecompy.viz.RMSE(actual, predicted)
+    
+    assert np.isclose(rmse, rmse_fx), "RMSE was incorrectly calculated."
+
+
 def test_muap_dict(fx_data, mu):
     """
     Run unit test on muap_dict function from EMGdecomPy.
